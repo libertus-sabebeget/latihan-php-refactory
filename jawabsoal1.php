02d831acc9454ac8337658388547a6e066693b11
libertus-sabebeget <91246440+libertus-sabebeget@users.noreply.github.com>
2021-09-23 14:15:34 +0700

    Add files via upload

---
+++ jawabsoal1.php
@@ -0,0 +1,8 @@
+<?php
+
+   $nomor = 1;
+   for( $ucapan = 1; $ucapan <=10; $ucapan++ ) {
+      echo $nomor++.") "."Glory-Glory Refactory <br/>";
+   }
+
+?>
---
+++ jawabsoal2.php
@@ -0,0 +1,10 @@
+<?php 
+
+   $uangSaya = 10000;
+   $hargaNasgor = 15000;
+
+   if( $hargaNasgor > $uangSaya ) {
+      echo "Uang anda kurang dari harga nasi gorengnya";
+   }
+
+?>
